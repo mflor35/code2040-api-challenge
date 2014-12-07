3c77daf2da8e7e496cfc6db53b5762a1ac107c49
Miguel Flores Silverio <miguel@D8Y510P>
2014-12-07 00:16:10 -0800

    Fixed formatting on comments. Gotta make everything look clean. :)

--- reverse_string.py
+++ reverse_string.py
@@ -1,7 +1,7 @@
-#Stage I: Rervese a string
+# Stage I: Rervese a string
 import urllib2
 import json
-#Connect to the API and get challenge
+# Connect to the API and get challenge
 challenge_url  = 'http://challenge.code2040.org/api/getstring'
 info           = {'token':'rmUQt128vF'}
 request        = urllib2.Request(challenge_url)
@@ -14,7 +14,7 @@ content        = response.read()
 string         = json.loads(content)['result']
 reversed_str   = string[::-1]
 
-#submit reversed string to the API
+# Submit reversed string to the API
 submit_url     = 'http://challenge.code2040.org/api/validatestring'
 info           = {'token':'rmUQt128vF', 'string':reversed_str}
 request        = urllib2.Request(submit_url)
