fbf4cbdacb98c8ec6f86affdf9ab3adce6697f2e
Miguel Flores Silverio <miguel@D8Y510P>
2014-12-07 01:51:45 -0800

    Added a function to check my grade in the API. Also, formatted output a little bit.

--- needle_in_a_haystack.py
+++ needle_in_a_haystack.py
@@ -30,6 +30,16 @@ def findneedle(data):
             return {'token':'rmUQt128vF','needle':index}
         index += 1
 # End of solution to the challenge
+def getGrades(token):
+    info           = {'token':token}
+    request        = urllib2.Request('http://challenge.code2040.org/api/status')
+    request.add_header('Conten-Type', 'application/json')
+    response       = urllib2.urlopen(request, json.dumps(info))
+    content        = response.read()
+    result         = json.loads(content)['result']
+    print "============== Grades ========================"
+    for challenge in result:
+        print challenge + '\t\t' + str(result[challenge])
 
 # Main or driving function
 def main():
@@ -37,5 +47,8 @@ def main():
     challenge_data = getchallenge(my_token,'http://challenge.code2040.org/api/haystack')
     answer_data    = findneedle(challenge_data)
     validation     = validatechallenge(answer_data,'http://challenge.code2040.org/api/validateneedle')
+
+    print "============== API response =================="
     print json.loads(validation)['result']
+    getGrades(my_token)
 main()
