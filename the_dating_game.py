c24bc172f67633eaa38fa02739296a5e4f111ee0
Miguel Flores Silverio <floresmigu3l@gmail.com>
2014-12-08 02:07:09 -0800

    Solution for The dating game.

--- the_dating_game.py
+++ the_dating_game.py
@@ -1,5 +1,8 @@
 import urllib2
 import json
+import iso8601 # Needs to be installed in the system before running the script.
+import datetime # http://pyiso8601.readthedocs.org/en/latest/#parsed-formats
+
 #  getchallenge takes in a token and url and returns a sring with the challenge data
 def getchallenge(token,url):
     info           = {'token':token}
@@ -19,8 +22,13 @@ def validatechallenge(answer_dict,url):
     return content
 
 # Start of solution to the challenge
-def prefix(data,token):
-    return data
+def datingGame(data,token):
+    result         = json.loads(data)['result']
+    datestamp      = result['datestamp']
+    interval       = result['interval']
+    time           = iso8601.parse_date(datestamp)
+    time           = time + datetime.timedelta(seconds=interval)
+    return {'token':token, 'datestamp':str(time.isoformat())}
 # End of solution to the challenge
 def getGrades(token):
     info           = {'token':token}
@@ -37,10 +45,10 @@ def getGrades(token):
 def main():
     my_token       = 'rmUQt128vF'
     challenge_data = getchallenge(my_token,'http://challenge.code2040.org/api/time')
-    answer_data    = prefix(challenge_data,my_token)
-    #validation     = validatechallenge(answer_data,'http://challenge.code2040.org/api/validatetime')
+    answer_data    = datingGame(challenge_data,my_token)
+    validation     = validatechallenge(answer_data,'http://challenge.code2040.org/api/validatetime')
     print "============== API response =================="
-    #print json.loads(validation)['result']
-    print json.loads(answer_data)['result']
-    #getGrades(my_token)
+    print json.loads(validation)['result']
+    #print answer_data
+    getGrades(my_token)
 main()
