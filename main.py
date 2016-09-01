5045fe06c90f68f35807de1fb4c8aed5f66aa266
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-09-01 11:05:53 -0700

    Get rid of the menu just automate everything.

--- main.py
+++ main.py
@@ -1,58 +1,49 @@
+#! /bin/python3
 import stages
-import json
 import CODE2040API
-# Main or driving function
+
+
+def find_solution(url, data, token):
+
+    if(url.find('reverse') > 0):
+        return stages.reverse_string(data, token)
+    elif(url.find('haystack') > 0):
+        return stages.findneedle(data, token)
+    elif(url.find('prefix') > 0):
+        return stages.prefix(data, token)
+    elif(url.find('dating') > 0):
+        return stages.datingGame(data, token)
+    return -1
+
+
 def main():
-    my_token       = 'rmUQt128vF'
+    registration_url = 'http://challenge.code2040.org/api/register'
+    status_url = 'http://challenge.code2040.org/api/status'
+    token = '174f74259da7236f9a2011c1c0b68511'
+    steps_urls = [
+        'http://challenge.code2040.org/api/reverse',
+        'http://challenge.code2040.org/api/haystack',
+        'http://challenge.code2040.org/api/prefix',
+        'http://challenge.code2040.org/api/dating'
+    ]
 
-    challenge_urls = [
-            'http://challenge.code2040.org/api/getstring',
-            'http://challenge.code2040.org/api/prefix',
-            'http://challenge.code2040.org/api/time',
-            'http://challenge.code2040.org/api/haystack',
-            ]
+    validation_urls = [
+        'http://challenge.code2040.org/api/reverse/validate',
+        'http://challenge.code2040.org/api/haystack/validate',
+        'http://challenge.code2040.org/api/prefix/validate',
+        'http://challenge.code2040.org/api/dating/validate',
+    ]
 
-    validation_urls= [
-            'http://challenge.code2040.org/api/validatestring',
-            'http://challenge.code2040.org/api/validateprefix',
-            'http://challenge.code2040.org/api/validatetime',
-            'http://challenge.code2040.org/api/validateneedle'
-            ]
+    print("Solving Challenges")
 
-    valid_choice = True
-    while(valid_choice == True):
-        print("******************************************")
-        print("Select Challenge to be solved:           *")
-        print("1.- Reverse String                       *")
-        print("2.- Prefix                               *")
-        print("3.- The Dating Game                      *")
-        print("4.- Haystack                             *")
-        print("                                         *")
-        print("Enter 'Grades' to check grade on the API *")
-        print("******************************************")
-        user_choice = input("Enter choice: ")
-
-        if user_choice == '1':
-            challenge_data   = CODE2040API.getchallenge(my_token,challenge_urls[0])
-            challenge_answer = stages.reversed_string(challenge_data,my_token)
-            print(CODE2040API.validatechallenge(challenge_answer,validation_urls[0]))
-
-        elif user_choice == '2':
-            challenge_data   = CODE2040API.getchallenge(my_token,challenge_urls[1])
-            challenge_answer = stages.prefix(challenge_data,my_token)
-            print(CODE2040API.validatechallenge(challenge_answer,validation_urls[1]))
-
-        elif user_choice == '3':
-            challenge_data   = CODE2040API.getchallenge(my_token,challenge_urls[2])
-            challenge_answer = stages.datingGame(challenge_data,my_token)
-            print(CODE2040API.validatechallenge(challenge_answer,validation_urls[2]))
-
-        elif user_choice == '4':
-            challenge_data   = CODE2040API.getchallenge(my_token,challenge_urls[3])
-            challenge_answer = stages.findneedle(challenge_data,my_token)
-            print(CODE2040API.validatechallenge(challenge_answer,validation_urls[3]))
-        elif user_choice.lower() == 'grades':
-            CODE2040API.getGrades(my_token)
+    for i in range(0, len(steps_urls)):
+        data = CODE2040API.getchallenge(token, steps_urls[i])
+        solution = find_solution(steps_urls[i], data, token)
+        if(solution != -1):
+            print(solution)
+            print(CODE2040API.validatechallenge(solution, validation_urls[i]))
         else:
-            valid_choice = False
+            print("solution not found")
+
+    print(CODE2040API.status(status_url, token))
 main()
